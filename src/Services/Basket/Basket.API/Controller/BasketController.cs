b7f7dd723c75546723bf97cc6b376f3c4578d959
Guzel278 <guzel95@list.ru>
2021-12-12 19:46:12 +0300

    integrate Discount.Grpc into DC

--- src/Services/Basket/Basket.API/Controller/BasketController.cs
+++ src/Services/Basket/Basket.API/Controller/BasketController.cs
@@ -1,4 +1,5 @@
 ﻿using Basket.API.Entities;
+using Basket.API.GrpcServices;
 using Basket.API.Repositories;
 using Microsoft.AspNetCore.Mvc;
 using System.Net;
@@ -10,10 +11,12 @@ namespace Basket.API.Controller
     public class BasketController :ControllerBase    
     {
         private readonly IBasketRepository repository;
+        private readonly DiscountGrpcService discountGrpcService;
 
-        public BasketController(IBasketRepository repository)
+        public BasketController(IBasketRepository repository, DiscountGrpcService discountGrpcService) 
         {
             this.repository = repository ?? throw new ArgumentNullException(nameof(repository));
+            this.discountGrpcService = discountGrpcService ?? throw new ArgumentNullException(nameof(discountGrpcService));
         }
 
         [HttpGet("{userName}",Name ="GetBasket")]
@@ -27,6 +30,14 @@ namespace Basket.API.Controller
         [ProducesResponseType(typeof(void), (int)HttpStatusCode.OK)]
         public async Task<ActionResult<ShoppingCart>> UpdateBasket([FromBody] ShoppingCart basket)
         {
+            //TODO: Communicate with Discount.Grpc 
+            //and Calculate lastest prices of product into shopping cart
+            // consume Discount Grpc
+            foreach (var item in basket.Items)
+            {
+                var coupon = await discountGrpcService.GetDiscount(item.ProductName);
+                item.Price -= coupon.Amount;
+            }
             return Ok(await repository.UpdateBasket(basket));
         }
 
--- src/Services/Basket/Basket.API/Startup.cs
+++ src/Services/Basket/Basket.API/Startup.cs
@@ -2,7 +2,9 @@
 //using Basket.API.Repositories;
 //using Discount.Grpc.Protos;
 //using MassTransit;
+using Basket.API.GrpcServices;
 using Basket.API.Repositories;
+using Discount.Grpc.Protos;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -36,9 +38,9 @@ namespace Basket.API
            // services.AddAutoMapper(typeof(Startup));
 
             // Grpc Configuration
-            //services.AddGrpcClient<DiscountProtoService.DiscountProtoServiceClient>
-            //    (o => o.Address = new Uri(Configuration["GrpcSettings:DiscountUrl"]));
-            //services.AddScoped<DiscountGrpcService>();
+            services.AddGrpcClient<DiscountProtoService.DiscountProtoServiceClient>
+                (o => o.Address = new Uri(Configuration["GrpcSettings:DiscountUrl"]));
+            services.AddScoped<DiscountGrpcService>();
 
             // MassTransit-RabbitMQ Configuration
             //services.AddMassTransit(config => {
