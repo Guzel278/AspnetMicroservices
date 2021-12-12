67c11064269429035938a7ad1b87eb122500c813
Guzel278 <guzel95@list.ru>
2021-12-12 04:25:14 +0300

    added Discount.Grpc

---
+++ src/Services/Discount/Discount.Grpc/Repositories/IDiscountRepository.cs
@@ -0,0 +1,15 @@
+﻿using Discount.Grpc.Entities;
+using Discount.Grpc.Protos;
+
+namespace Discount.Grpc.Repositories
+{
+    public interface IDiscountRepository
+    {
+        Task<Coupon> GetDiscount(string productName);
+        Task<bool> CreateDiscount(Coupon coupon);
+        Task<bool> UpdateDiscount(Coupon coupon);
+        Task<bool> DeleteDiscount(string productName);
+        Task UpdateDiscount(CouponModel coupon);
+        Task CreateDiscount(CouponModel coupon);
+    }
+}
